6dabbe60dffc2b930a5cf3653fe8df83b9ab8914
NSPatton <greengun15@gmail.com>
2021-03-12 10:23:52 -0600

    added notes

--- scripts/legos/LegoDetail.js
+++ scripts/legos/LegoDetail.js
@@ -4,6 +4,7 @@ export const LegoDetail = (brick) => {
 
   let block = `<section class="block-wrapper" style="background-color:#${brick.ColorHex}">
               <h3>Name: ${brick.LegoName}</h3>
+              <h4>Notes: ${brick.Notes}</h4>
               <div class="block-years">Manufactured ${brick.YearFrom} - ${brick.YearTo}</div>
               `;
   const link = brick.ColorstreamLinkImage;
